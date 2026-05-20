7cea47d35011e53458f58a69bf97e878caf2c343
gity678 <philipegau6@gmail.com>
2026-05-20 14:50:52 +0100

    Create app/src/main/java/com/example/musicapp/MainActivity.java

---
+++ app/src/main/java/com/example/musicapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.musicapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMusic = findViewById(R.id.btnMusic);
+        Button btnRadio = findViewById(R.id.btnRadio);
+        Button btnYoutube = findViewById(R.id.btnYoutube);
+
+        btnMusic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // سنضيف MusicActivity لاحقاً
+            }
+        });
+
+        btnRadio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // سنضيف RadioActivity لاحقاً
+            }
+        });
+
+        btnYoutube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // سنضيف YouTubeActivity لاحقاً
+            }
+        });
+    }
+}
